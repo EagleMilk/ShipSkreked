b3b820de31aff96b8caa7fa105f1bebe9ef4da40
andoni.sanguesa <andoni.sanguesa@e3civichigh.com>
2018-03-07 15:09:07 -0800

    Added item descriptors along with a inspect function (currently in the wrong place). Also added a wallet object.

--- adventure.py
+++ adventure.py
@@ -1,5 +1,6 @@
 from Room import Room
 from inventory import Inventory
+from inventory import Wallet
 
 print '   _____ __                       __         __\n  / ___// /__________ _____  ____/ /__  ____/ /\n  \__ \/ __/ ' \
       '___/ __ `/ __ \/ __  / _ \/ __  / \n ___/ / /_/ /  / /_/ / / / / /_/ /  __/ /_/ /  \n/____/\__/_/   \__,' \
@@ -7,9 +8,10 @@ print '   _____ __                       __         __\n  / ___// /__________ __
 
 
 Beach = Room('Beach', "You find yourself awake on a beach - palm trees swaying in the wind, the ocean breeze upon "
-                      "your face, and white sand all around you. You see half of a ship lodged between two rocks and "
-                      "a large opening facing you. There is a wallet on the floor about a yard away", 1)
-stop = Room('no', 'stop', 2)
+                      "your face, and white sand all around you. \nYou see half of a cruise ship lodged between two rocks"
+                      " and a large opening facing you. There is a wallet on the floor about a yard away", 1)
+stop = Room('Ship Entrance', 'You walk into the large hole in the cruise ship and are in a vast dark space, very'
+                             'lightly lit by the outside world. ', 2)
 hallway = Room('hallway', 'you are in the hallway', 3)
 hallway2 = Room('upstairs hallway', "You are in the upstairs hallway", 4)
 bedroom = Room('bedroom', 'you are in the bedroom', 5)
@@ -20,6 +22,7 @@ living = Room('living room', 'you are in the living room', 8)
 
 inventory = Inventory()
 current_room = Beach
+Beach.add_item(Wallet('Wallet', 'This is a test'))
 
 while True:
     current_room.enter_room()
--- inventory.py
+++ inventory.py
@@ -1,3 +1,6 @@
+from Room import Room
+
+
 class Inventory():
     def __init__(self):
         self.items = []
@@ -5,17 +8,42 @@ class Inventory():
     def add(self, item):
         self.items.append(item)
 
+    def drop(self, item):
+        self.items.remove(item)
+
     def list(self):
-        print 'You are carrying:'
+        print ("You are carrying:")
         for item in self.items:
-            print item.get_name()
+            print (item.get_name())
+
+    def get(self, type):
+        items_of_type = []
+        for item in self.items:
+            if isinstance(item, type):
+                items_of_type.append(item)
+        return items_of_type
+
+    def process_command(self, command):
+        result = []
+        for item in self.items:
+            if item.get_name() in command:
+                result.append(item.process_command(command))
+        return result
+
 
 class Item():
-    def __init__(self, name):
+    def __init__(self, name, description):
         self.name = name
+        self.known_commands = {}
+        self.itemsdescriptions = {name: description}
 
     def get_name(self):
         return self.name
+
+    def process_command(self, command):
+        for a_command in self.known_commands:
+            if a_command in command:
+                self.known_commands[a_command](command)
 
 
 class Literature(Item):
@@ -28,6 +56,7 @@ class Literature(Item):
 
     def set_contents(self, contents):
         self.contents = contents
+
 
 class Flashlight(Item):
     def __init__(self, name, battery_level=100, state='off'):
@@ -47,3 +76,62 @@ class Flashlight(Item):
 
     def compute_usage(self):
         pass
+
+
+
+class LightSource(Item):
+    def __init__(self, name, on=False):
+        self.on = on
+        Item.__init__(self, name)
+        self.known_commands["turn on"] = self.turn_on
+        self.known_commands["turn off"] = self.turn_off
+
+    @staticmethod
+    def is_one_on(sources):
+        if len(sources) > 0:
+            for source in sources:
+                if source.is_on():
+                    return True
+        return False
+
+    def is_on(self):
+        return self.on
+
+    def turn_on(self, command):
+        self.on = True
+        print ("The " + self.name + " is on.")
+
+    def turn_off(self, command):
+        self.on = False
+        print ("The " + self.name + " is off.")
+
+class Wallet(Item):
+    def __init__(self, name, description):
+        Item.__init__(self, name, description)
+        self.known_commands["inspect"] = self.inspect()
+
+    def inspect(self):
+        return self.itemsdescriptions[self.name]
+
+
+class Flashlight(LightSource):
+    def __init__(self, name="flashlight", battery_level=100, on=False):
+        LightSource.__init__(self, name, on)
+        self.battery_level = battery_level
+
+    def change_batteries(self):
+        self.battery_level=100
+
+    def compute_usage(self):
+        # Compute the time it's been on and then drain the battery an equal amount
+        pass
+
+class DarkRoom(Room):
+    def enter_room(self, inventory):
+        light_sources = inventory.get(LightSource)
+        if LightSource.is_one_on(light_sources):
+            Room.enter_room(self, inventory)
+        else:
+            print ("You were eaten by a grue.")
+            print ("Game over.")
+            exit()
